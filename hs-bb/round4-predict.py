5b658e8caaa18b5d555ffb6b6dad9859bb1316a1
radiocat <radiocat.app@gmail.com>
2016-08-18 18:39:44 +0900

    準々決勝の結果を反映

--- hs-bb/round4-predict.py
+++ hs-bb/round4-predict.py
@@ -10,10 +10,10 @@ Y = df['得点'].as_matrix()
 clf = linear_model.LinearRegression()
 clf.fit(X, Y)
 
-# 3回戦の予測
-df_round3 = pd.read_csv('round4-game-predict-2016.csv')
-X_round3 = df_round3.drop(['県','県No','学校名','対戦校','予想得点力'], axis=1)
-round3_pred=clf.predict(X_round3)
+# 4回戦の予測
+df_next = pd.read_csv('round4-game-predict-2016.csv')
+X_next = df_next.drop(['県','県No','学校名','対戦校','予想得点力','結果','差'], axis=1)
+next_pred=clf.predict(X_next)
 
-print(round3_pred)
+print(next_pred)
 
