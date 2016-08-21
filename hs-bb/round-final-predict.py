66030438098de16ef96ec65790a49ea5875b0f8f
radiocat <radiocat.app@gmail.com>
2016-08-21 11:59:25 +0900

    決勝戦の予測

--- hs-bb/round-final-predict.py
+++ hs-bb/round-final-predict.py
@@ -3,15 +3,15 @@ import pandas as pd
 import numpy as np
 from sklearn import linear_model
 
-df = pd.read_csv('round1-4-study-2016.csv')
+df = pd.read_csv('round1-5-study-2016.csv')
 X = df.drop(['県','県No','学校名','対戦校','得点'], axis=1)
 Y = df['得点'].as_matrix()
 
 clf = linear_model.LinearRegression()
 clf.fit(X, Y)
 
-# 準決勝の予測
-df_next = pd.read_csv('round5-game-predict-2016.csv')
+# 決勝の予測
+df_next = pd.read_csv('round6-game-predict-2016.csv')
 X_next = df_next.drop(['県','県No','学校名','対戦校','予想得点力','結果','差'], axis=1)
 next_pred=clf.predict(X_next)
 
